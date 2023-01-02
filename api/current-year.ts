c257e184392e35906cb51713b5f668b558b0feeb
Alan Russell <ajrussellaudio@gmail.com>
2023-01-02 18:28:50 +0000

    Ensure year has at least 4 digits

--- api/current-year.ts
+++ api/current-year.ts
@@ -1,9 +1,11 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import leftPad from "left-pad";
 
 export default (req: VercelRequest, res: VercelResponse) => {
   const timeZone = (req.query.tz as string) || "America/Chicago";
   const dateString = new Date().toLocaleString("en-US", { timeZone });
   const date = new Date(dateString);
+  const year = date.getFullYear();
 
-  return res.send(date.getFullYear());
+  return res.send(leftPad(year, 4));
 };
