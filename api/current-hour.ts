bd3ff8d82d6d1fccc84a6bb01fa28a5f1db9410d
wesbragagt <weslley@bambee.com>
2023-01-10 16:25:57 -0600

    adds: endpoint for current hour

---
+++ api/current-hour.ts
@@ -0,0 +1,8 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { getDateForTimezone } from "./_utils";
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  const date = getDateForTimezone(req.query.tz as string);
+
+  return res.send(String((req.query.mockDate as string) || date.getHours()));
+};
